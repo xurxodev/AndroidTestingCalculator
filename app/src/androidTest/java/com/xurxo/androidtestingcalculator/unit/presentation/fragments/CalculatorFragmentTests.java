1a2c706fa98f846d3b42cd55ca02336704c33b0b
xurxosf <xurxosf@gmail.com>
2015-08-28 13:16:14 +0200

    add instrumentation test sample

---
+++ app/src/androidTest/java/com/xurxo/androidtestingcalculator/unit/presentation/fragments/CalculatorFragmentTests.java
@@ -0,0 +1,31 @@
+package com.xurxo.androidtestingcalculator.unit.presentation.fragments;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.Button;
+
+import com.xurxo.androidtestingcalculator.R;
+import com.xurxo.androidtestingcalculator.unit.fragments.presentation.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class CalculatorFragmentTests {
+
+    @Rule
+    public ActivityTestRule mainActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void customIntentToStartActivity() {
+
+        Button equalButton = (Button) mainActivityRule.getActivity().findViewById(R.id.equal_button);
+
+        assertThat(equalButton.getText().toString(), is("="));
+    }
+}
