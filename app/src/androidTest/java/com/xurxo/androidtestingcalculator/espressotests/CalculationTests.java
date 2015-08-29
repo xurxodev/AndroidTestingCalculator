d9e414ff341619cf303816984b84675913ba848d
xurxosf <xurxosf@gmail.com>
2015-08-29 21:48:17 +0200

    add espresso test

--- app/src/androidTest/java/com/xurxo/androidtestingcalculator/espressotests/CalculationTests.java
+++ app/src/androidTest/java/com/xurxo/androidtestingcalculator/espressotests/CalculationTests.java
@@ -1,4 +1,33 @@
 package com.xurxo.androidtestingcalculator.espressotests;
 
+import android.support.test.rule.ActivityTestRule;
+
+import com.xurxo.androidtestingcalculator.R;
+import com.xurxo.androidtestingcalculator.unit.fragments.presentation.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
 public class CalculationTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ClickButtonOne_DisplayShouldShowOne(){
+        onView(withId(R.id.one_button))
+                .perform(click());
+
+        onView(withId(R.id.display_TextView))
+                .check(matches(withText("1")));
+
+
+    }
 }
