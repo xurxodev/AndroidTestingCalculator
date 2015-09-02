a498e9beddb836ab4c5cd162574d363baf54dfea
xurxosf <xurxosf@gmail.com>
2015-09-02 11:42:10 +0200

    add espresso tests

--- app/src/androidTest/java/com/xurxo/androidtestingcalculator/espressotests/InitialStateTests.java
+++ app/src/androidTest/java/com/xurxo/androidtestingcalculator/espressotests/InitialStateTests.java
@@ -2,6 +2,9 @@ package com.xurxo.androidtestingcalculator.espressotests;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.xurxo.androidtestingcalculator.R;
 import com.xurxo.androidtestingcalculator.presentation.activities.MainActivity;
@@ -12,8 +15,12 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class InitialStateTests {
@@ -22,9 +29,47 @@ public class InitialStateTests {
     public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
+    public void ToStartApp_TitleShouldBeCalculator() {
+
+        //ToolBar-> TextView
+        //Search TexView inside toolbar with text calculator
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Calculator")));
+    }
+
+    @Test
     public void ToStartApp_DisplayShouldBeEmpty(){
 
         onView(withId(R.id.display_TextView))
                 .check(matches(withText("")));
+    }
+
+    @Test
+    public void ToStartApp_NumberButtonsShouldBeDisplayed(){
+        for(int i=0; i<10; i++){
+            onView(allOf(isAssignableFrom(Button.class), withText(String.valueOf(i))))
+                    .check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void ToStartApp_OperationsButtonsShouldBeDisplayed(){
+        onView(allOf(isAssignableFrom(Button.class), withText("+")))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(isAssignableFrom(Button.class), withText("-")))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(isAssignableFrom(Button.class), withText("x")))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(isAssignableFrom(Button.class), withText("/")))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(isAssignableFrom(Button.class), withText("%")))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(isAssignableFrom(Button.class), withText("√")))
+                .check(matches(isDisplayed()));
     }
 }
